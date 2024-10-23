7217cd536a4ad37d7f749595ef2626d2c6bc1e95
umaedi <humaedi.kh.99@gmail.com>
2024-10-23 11:00:09 +0700

    burger

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -63,7 +63,7 @@
         </a>
       </div>
 
-      <button class="navbar-toggler shadow-none mt-2" type="button" data-bs-toggle="offcanvas"
+      <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="offcanvas"
         data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
         <span class="navbar-toggler-icon"></span>
       </button>
